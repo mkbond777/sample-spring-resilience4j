d710b7cbbf4688609c620d4d9101744694098767
manish248.kumar <Sonali@2616608>
2023-01-04 15:29:14 +0530

    Working code with http-client library having resilience4j dependencies.

--- src/main/java/com/jio/sample/SampleApplication.java
+++ src/main/java/com/jio/sample/SampleApplication.java
@@ -3,7 +3,8 @@ package com.jio.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.jio.commons.httpclients","com.jio.sample"})
+// https://youtrack.jetbrains.com/issue/IDEA-281454/Could-not-autowire.-No-beans-of-XXX-type-found-IntelliJ-2021.2.3-Spring-2.5.6
 public class SampleApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jio/sample/controller/SampleController.java
+++ src/main/java/com/jio/sample/controller/SampleController.java
@@ -1,5 +1,9 @@
 package com.jio.sample.controller;
 
+import com.jio.commons.httpclients.models.ClientObj;
+import com.jio.commons.httpclients.models.HttpMethods;
+import com.jio.commons.httpclients.models.ResponseSync;
+import com.jio.commons.httpclients.okhttp.OkHttp3;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sample")
 public class SampleController {
 
-    @CircuitBreaker(name="backendB")
+    private final OkHttp3 okHttp3;
+
+    public SampleController(OkHttp3 okHttp3) {
+        this.okHttp3 = okHttp3;
+    }
+
     @GetMapping("/get")
     public String getData() {
-        return "API controller";
+
+        ClientObj clientObj = ClientObj.builder()
+                .retryCount(2)
+                .build();
+        record GetResponse(Boolean success) {
+        }
+
+        ResponseSync<GetResponse> responseSync = okHttp3
+                .httpCallSync("https://reqbin.com/echo/get/json", null,
+                        GetResponse.class, HttpMethods.GET,clientObj);
+
+        return responseSync.toString();
     }
 
 }
